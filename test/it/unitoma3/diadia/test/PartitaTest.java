cee6e72fac333425432e2272c16f47aee0cef870
Simo2310 <smnmntnr@gmail.com>
2025-03-30 10:58:43 +0200

    test Partita Done
    bisogna implementare il metodo isFinita dentro Partita, che non viene mai chiamato in tutto il progetto

--- test/it/unitoma3/diadia/test/PartitaTest.java
+++ test/it/unitoma3/diadia/test/PartitaTest.java
@@ -1,5 +1,42 @@
 package it.unitoma3.diadia.test;
 
-public class PartitaTest {
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import it.uniroma3.diadia.Partita;
+
+public class PartitaTest {
+	
+	public Partita partita = new Partita();
+	
+	@BeforeEach
+	public void setUp() {
+		
+	}
+	
+	
+	@Test
+	public void testVinta_inAtrio() {
+		assertFalse(this.partita.vinta());
+	}
+	
+	@Test
+	public void testVinta_inBiblioteca() {
+		this.partita.getLabirinto().setStanzaCorrente(this.partita.getLabirinto().getStanzaVincente());
+		assertTrue(this.partita.vinta());
+	}
+	
+	@Test
+	public void testIsFinita_nonFinita() {
+		assertFalse(this.partita.vinta());
+	}
+	
+	@Test
+	public void testIsFinita_Xcfu() {
+		this.partita.getGiocatore().setCfu(0);
+		assertTrue(this.partita.isFinita());
+	}
 }
