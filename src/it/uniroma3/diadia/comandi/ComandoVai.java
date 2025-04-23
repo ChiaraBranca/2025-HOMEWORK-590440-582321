29beb2477eb24e3e1cf3481db2509989e1fda8b6
ChiaraBranca <lisabranca11@gmail.com>
2025-04-23 15:02:13 +0200

    prima parte codice comandovai

--- src/it/uniroma3/diadia/comandi/ComandoVai.java
+++ src/it/uniroma3/diadia/comandi/ComandoVai.java
@@ -1,5 +1,20 @@
 package it.uniroma3.diadia.comandi;
 
-public class ComandoVai {
+import it.uniroma3.diadia.Partita;
+
+public class ComandoVai implements Comando {
+	private String direzione;
+
+	public ComandoVai(String direzione) {
+	this.direzione = direzione;
+	}
+
+	/**
+	* esecuzione del comando
+	*/
+	@Override
+	public void esegui(Partita partita) {
+	// qui il codice per cambiare stanza ...
+	}
 
 }
