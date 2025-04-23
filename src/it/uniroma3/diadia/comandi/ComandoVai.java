25266f64423ad94c0b9994cb946b16024b3cb085
Simo2310 <smnmntnr@gmail.com>
2025-04-23 15:34:48 +0200

    interface comandi e classi comandovai

--- src/it/uniroma3/diadia/comandi/ComandoVai.java
+++ src/it/uniroma3/diadia/comandi/ComandoVai.java
@@ -1,5 +1,34 @@
 package it.uniroma3.diadia.comandi;
 
-public class ComandoVai {
+import it.uniroma3.diadia.Partita;
+import it.uniroma3.diadia.ambienti.*;
+
+public class ComandoVai implements Comando{
+
+	private String direzione;
+	public ComandoVai(String direzione) {
+		this.direzione = direzione;
+	}
+	/**
+	 * esecuzione del comando
+	 */
+	@Override
+	public void esegui(Partita partita){
+
+		Stanza stanzaCorrente = partita.getLabirinto().getStanzaCorrente(); 
+		Stanza prossimaStanza = null; 
+		if (direzione==null) { 
+			System.out.println("Dove vuoi andare? Devi specificare una direzione"); 
+			return; 
+		} 
+		prossimaStanza = stanzaCorrente.getStanzaAdiacente(this.direzione); 
+		if (prossimaStanza==null) { 
+			System.out.println("Direzione inesistente"); 
+			return; 
+		} 
+		partita.getLabirinto().setStanzaCorrente(prossimaStanza); 
+		System.out.println(partita.getLabirinto().getStanzaCorrente().getNome()); 
+		partita.getGiocatore().setCfu (partita.getGiocatore().getCfu()-1); 
+	}
 
 }
